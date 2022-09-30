a93099c7fab428b6553b2b9f4d755155bfc263c1
Carson <jackparadise520a@gmail.com>
2022-09-30 11:24:11 +0800

    添加更多主程式的測試案例 (#4)

--- src/main_test.go
+++ src/main_test.go
@@ -2,8 +2,59 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 )
+
+func chdir(dir string) (back2orgDirFunc func()) {
+	orgDir, _ := os.Getwd()
+	_ = os.Chdir(dir) // 注意test會跑所有Example, Test的案例，所以當有chdir用的是相對路徑，那麼就要確保相對路徑都是正確的
+	return func() {
+		err := os.Chdir(orgDir)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func Example_main_caseSensitive() {
+	back2orgDirFunc := chdir("test/case_sensitive")
+	defer back2orgDirFunc()
+	os.Args = []string{os.Args[0], "-f=config.json", "-dry=1"}
+	main()
+	// Output:
+	// H?ll? World
+}
+
+func Example_main_caseInsensitive() {
+	back2orgDirFunc := chdir("test/case_insensitive")
+	defer back2orgDirFunc()
+	os.Args = []string{os.Args[0], "-f=config.json", "-dry=1"}
+	main()
+	// Output:
+	// H?ll? W?rld
+}
+
+// multiline影響的是匹配開頭的^或者結尾的$符號才會需要用到
+func Example_main_multiline() {
+	back2orgDirFunc := chdir("test/multiline")
+	defer back2orgDirFunc()
+	os.Args = []string{os.Args[0], "-f=config.json", "-dry=1"}
+	main()
+	// Output:
+	// Hello World foo
+	// Hello World
+}
+
+func Example_mainPattern() {
+	back2orgDirFunc := chdir("test/pattern")
+	defer back2orgDirFunc()
+	os.Args = []string{os.Args[0], "-f=config.json", "-dry=1"}
+	main()
+	// Output:
+	// Example0
+	// Example00
+}
 
 func Example_core() {
 	const testContent = `
